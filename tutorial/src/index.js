5bf0449d24019502f34b2a0b6231769fe8c24852
daseulll <nldaseul@gmail.com>
2021-07-04 10:57:22 +0900

    Added BooList, Book, Image, Title, Author components
    to clone amazon selling book list

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -5,22 +5,33 @@ import ReactDom from "react-dom";
 // Component를 작성하기 위해서는 먼저, React를 import 해야한다.(from 'react')
 // 정식명칭 : stateless functional component
 // always return JSX
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>daseul</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
 };
-
+const Image = () => (
+  <img
+    src='https://m.media-amazon.com/images/I/71eJk7GtUOL._AC_UY436_FMwebp_QL65_.jpg'
+    alt=''
+  />
+);
+const Title = () => <h1>The Boy, the Mole, the Fox and the Horse</h1>;
+const Author = () => <h4>Charlie Mackesy</h4>;
 // JSX를 실제로 render하기 위해서는, 아래와 같이 ReactDom.render함수에
 // ReactDom.render({무엇을 랜더할 것인지}, {어디에 랜더할 것인지}) 두가지 인자를 넘겨주어야 한다.
 // index.js는 js의 entry point로서, entry point 구조를 정의한다.
 // ReactDom.render는 원하는 js코드를 html에서 실행시키고 싶을 때 사용하는 함수이다.
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<BookList />, document.getElementById("root"));
