4a33994093604da01f23cf37b869a03397bc6d18
daseulll <nldaseul@gmail.com>
2021-07-06 07:35:57 +0900

    tmp

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -11,34 +11,26 @@ function BookList() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
-
+const author = "Charlie Mackesy";
 const Book = () => {
+  const title = "The Boy, the Mole, the Fox and the Horse";
   return (
     <article className='book'>
-      <Image></Image>
-      <Title />
-      <Author />
+      <img
+        src='https://m.media-amazon.com/images/I/71eJk7GtUOL._AC_UY436_FMwebp_QL65_.jpg'
+        alt=''
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {/* <p>{let x = 6}</p> */}
+      <p>{6 + 6}</p>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src='https://m.media-amazon.com/images/I/71eJk7GtUOL._AC_UY436_FMwebp_QL65_.jpg'
-    alt=''
-  />
-);
-const Title = () => <h1>The Boy, the Mole, the Fox and the Horse</h1>;
-const Author = () => <h4>Charlie Mackesy</h4>;
+
 // JSX를 실제로 render하기 위해서는, 아래와 같이 ReactDom.render함수에
 // ReactDom.render({무엇을 랜더할 것인지}, {어디에 랜더할 것인지}) 두가지 인자를 넘겨주어야 한다.
 // index.js는 js의 entry point로서, entry point 구조를 정의한다.
